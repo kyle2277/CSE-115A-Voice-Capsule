78066b0792b195790b06ac95849c2a0d43606ef0
kyle2277 <kylejwon@gmail.com>
2021-11-23 21:13:01 -0800

    Added new capsule glow animation. Capsule 'opened' flag is updated locally when a capsule is opened for the first time.

--- Voice-Capsule/lib/src/capsules.dart
+++ Voice-Capsule/lib/src/capsules.dart
@@ -28,9 +28,12 @@ class CapsulesSlide extends StatefulWidget {
 }
 
 
-class _CapsulesSlideState extends State<CapsulesSlide>{
+class _CapsulesSlideState extends State<CapsulesSlide> with SingleTickerProviderStateMixin {
   // List of available capsules
   List<VoiceCapsule> capsules = [];
+
+  late AnimationController newCapsuleAnimationController;
+  late Animation newCapsuleAnimation;
 
   // Initially load capsules from local .dat files
   Future<void> loadCapsules() async {
@@ -63,9 +66,23 @@ class _CapsulesSlideState extends State<CapsulesSlide>{
 
   @override
   @mustCallSuper
+  // Override widget initState to initialize animation and load capsules from local storage
   void initState() {
+    newCapsuleAnimationController = AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    newCapsuleAnimationController.repeat(reverse: true);
+    newCapsuleAnimation = Tween(begin: 0.95, end: 0.85).animate(newCapsuleAnimationController)..addListener(() {
+      setState(() {});
+    });
     loadCapsules();
     super.initState();
+  }
+
+  @override
+  @mustCallSuper
+  // Override widget dispose to terminate animation controller
+  void dispose() {
+    newCapsuleAnimationController.dispose();
+    super.dispose();
   }
 
   // Check if any capsules available, if so download from database
@@ -105,6 +122,22 @@ class _CapsulesSlideState extends State<CapsulesSlide>{
     ScaffoldMessenger.of(context).hideCurrentSnackBar();
   }
 
+  // Returns title of capsule depending on whether it has been opened yet
+  String getCapsuleTitle(VoiceCapsule capsule) {
+    if(capsule.opened) {
+      return "Capsule from";
+    } else {
+      return "NEW Capsule from";
+    }
+  }
+
+  // Color generator for new capsule glow animation
+  Color getNewCapsuleColor(double lightness) {
+    HSLColor col = HSLColor.fromColor(Colors.purple);
+    col = col.withLightness(lightness);
+    return col.toColor();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column (
@@ -116,44 +149,45 @@ class _CapsulesSlideState extends State<CapsulesSlide>{
             itemBuilder: (context, index) {
               return Card(
                 child: ListTile(
+                    tileColor: capsules[index].opened ? Colors.white : getNewCapsuleColor(newCapsuleAnimation.value),
                     trailing: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(
-                          icon: Icon(Icons.save_alt),
-                          iconSize: 22,
-                          onPressed: (() async {
-                            VoiceCapsule selected = capsules[index];
-                            await selected.saveToDownloads().then((success) {
-                              if(success) {
-                                showToast_quick(context, "Voice Capsule saved to downloads folder", duration:2);
-                              } else {
-                                showToast_quick(context, "Failed to download Voice Capsule", duration:2);
-                              }
-                            });
-                          }),
-                        ),
-                        IconButton(
-                          icon: Icon(Icons.delete),
-                          iconSize: 22,
-                          onPressed: () async {
-                            VoiceCapsule selected = capsules[index];
-                            String message = "If you've already opened it, selecting Yes will permanently delete this Voice Capsule from ${selected.senderName}.";
-                            bool? yes = await showAlertDialog_YESNO(context, "Are you sure?", message, textScale: 1.25);
-                            if(yes!) {
-                              await selected.delete().then((value) {
-                                setState(() {
-                                  capsules.remove(selected);
-                                  showToast_quick(context, "Voice Capsule deleted", duration:1);
-                                });
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.save_alt),
+                            iconSize: 22,
+                            onPressed: (() async {
+                              VoiceCapsule selected = capsules[index];
+                              await selected.saveToDownloads().then((success) {
+                                if(success) {
+                                  showToast_quick(context, "Voice Capsule saved to downloads folder", duration:2);
+                                } else {
+                                  showToast_quick(context, "Failed to download Voice Capsule", duration:2);
+                                }
                               });
-                            }
-                          },
-                        ),
-                      ]
+                            }),
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.delete),
+                            iconSize: 22,
+                            onPressed: () async {
+                              VoiceCapsule selected = capsules[index];
+                              String message = "If you've already opened it, selecting Yes will permanently delete this Voice Capsule from ${selected.senderName}.";
+                              bool? yes = await showAlertDialog_YESNO(context, "Are you sure?", message, textScale: 1.25);
+                              if(yes!) {
+                                await selected.delete().then((value) {
+                                  setState(() {
+                                    capsules.remove(selected);
+                                    showToast_quick(context, "Voice Capsule deleted", duration:1);
+                                  });
+                                });
+                              }
+                            },
+                          ),
+                        ]
                     ),
                     title:Text(
-                      "Capsule from",
+                      getCapsuleTitle(capsules[index]),
                       textScaleFactor: 0.9,
                     ),
                     subtitle: Text(
@@ -161,13 +195,16 @@ class _CapsulesSlideState extends State<CapsulesSlide>{
                       textScaleFactor: 1.5,
                     ),
                     onTap : () {
+                      setState(() {
+                        capsules[index].opened = true;
+                      });
                       Navigator.push(
-                      context,
-                      // Send audio file to player using capsules[index]
-                      MaterialPageRoute(builder: (context) => const PlaybackScreen()),
-                    );
-                  }
-                )
+                        context,
+                        // Send audio file to player using capsules[index]
+                        MaterialPageRoute(builder: (context) => const PlaybackScreen()),
+                      );
+                    }
+                ),
               );
             },
           ),
